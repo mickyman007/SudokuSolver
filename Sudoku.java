0c559701b7f6186d008af16b17b7461e192aba81
Benjy96 <benjamindickey@hotmail.co.uk>
2016-10-21 10:37:53 +0100

    Comment additions
    Explanation of the process in some methods, and re-arranged some code

--- Sudoku.java
+++ Sudoku.java
@@ -3,9 +3,15 @@ import java.util.*;
 /**
  * Created by Ben on 20/06/2016.
  * More comments incoming soon tm
+ 
+ Sudoku Rules:
+ - Must be no duplicate number in each row
+ - Must be no duplicate number in each column
+ - Must be no duplicate number in each grid (3x3)
  */
 public class Sudoku
 {
+    /* Sample sudoku table 9x9 */
     public static int[][] table = {
             {0, 1, 2, 0, 0, 0, 0, 7, 9},
             {0, 0, 9, 8, 3, 0, 0, 4, 0},
@@ -24,9 +30,14 @@ public class Sudoku
         System.out.println();
         printSudoku();
     }//main
+    
+    /* Executes method to go through sudoku table, params are simply starting position */
+    private static int solve(){
+        return countThrough(0, 0);
+    }
 
+    /* Prints the sudoku table (2D Array) */
     private static void printSudoku(){
-
         for(int i = 0; i < 9; i++){
             for(int j = 0; j < 9; j++){
                 System.out.print(table[i][j] + "\t");
@@ -35,41 +46,51 @@ public class Sudoku
         }
     }
 
+    /* Check if number already exists in row - r. We move along columns with i, while staying on same row. 
+    c - current position in static table 
+    num - num being tested */
     private static int rowCheck(int r, int c, int num){
-
+        /* c (column) is where the tested value is in the table, if we get there, simply break check 
+        e.g. If we inserted 5 into position 2 on table, skip or break that check, since it will return false and say there
+        are two of the same number on the same row (sudoku rule) */
         for(int i = 0; i < 9; i++)
         {
-            //skip row check if equal to the row in contemporary table ( to prevent duplicate returning 0
+            //skip check if equal to the position in contemporary table (to prevent duplicate returning 0)
             if(i == c && i < 8){
                 i++;
             }
 
-            //c is where the tested value is in the table, if we get there, simply break check
+            //reached end of row
             if(i == c && c == 8){
                 break;
             }
-            if (table[r][i] == num)
-            {
+            
+            //if table already has the number being tested, return false (sudoku rule)
+            if (table[r][i] == num){
                 return 0;
             }
         }
         return 1;
     }
 
+    //r - row - current position in static table
+    //c - column
+    //num - number being tested
+    //we move down by incrementing row with i, staying in same column
     private static int colCheck(int r, int c, int num){
-
         for(int i = 0; i < 9; i++)
         {
-            //skip col check if equal to the row in contemporary table ( to prevent duplicate returning 0
+            //skip check if equal to the position in contemporary table
             if(i == r && i < 8){
                 i++;
             }
 
-            //r is where the tested value is in the table, if we get there, simply break check
+            //reached end of column
             if(i == r && i == 8){
                 break;
             }
-
+            
+            //if table already has the tested number in the column, return false (sudoku rule)
             if (table[i][c] == num)
             {
                 return 0;
@@ -79,7 +100,6 @@ public class Sudoku
     }
 
     private static int panelCheck(int r, int c, int rmod, int cmod, int num){
-
         //Check row location, count from c -> c + 3
         for(int i = cmod; i < cmod+3; i++){
             //duplicate check - skips values from current table being tested
@@ -178,9 +198,5 @@ public class Sudoku
         //Unsuccessful, reset element
         table[r][c] = 0;
         return 0;
-    }
-
-    private static int solve(){
-        return countThrough(0, 0);
     }
 }//class
